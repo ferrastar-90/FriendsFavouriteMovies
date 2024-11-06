ad3e3e543df4c506650cab076a7fe5c94d6335fe
James Ferrari <james.ferrari@mac.com>
2024-11-06 21:10:54 +0000

    Initial Commit

---
+++ FriendsFavouriteMovies/FriendsFavouriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavouriteMoviesApp.swift
+//  FriendsFavouriteMovies
+//
+//  Created by James Ferrari on 6/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavouriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
